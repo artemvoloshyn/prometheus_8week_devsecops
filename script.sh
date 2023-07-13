d38a9616337d8340c4d8e37b9daf86b732257036
ar-volos <136924495+ar-volos@users.noreply.github.com>
2023-07-13 22:12:27 +0000

    add py script

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# From Source
+git clone https://github.com/gitleaks/gitleaks.git
+cd gitleaks
+make build
+
+chmod +x gitleaks
+sudo mv gitleaks /usr/local/bin/
+git clone 
+cp ../pre-commit.py ../.git/hooks/pre-commit
+chmod +x ../../.git/hooks/pre-commit
+git config --global --bool hooks.gitleaks true
+
+echo 'gitleaks is installed'
+
+# GITLEAKS_BASE_DIR=gitleaks-precommit
+# GITLEAKS_REPO=https://github.com/gitleaks/gitleaks.git
+# REPO_DIR=gitleaks
+
+# mkdir -p $GITLEAKS_BASE_DIR
+# cd $GITLEAKS_BASE_DIR
+
+# if [ -d $REPO_DIR ]; then
+#     echo "removing existing directory '$REPO_DIR'"
+#     rm -rf $REPO_DIR
+# fi
+
+# git clone $GITLEAKS_REPO
+# cd $REPO_DIR
+# make build
+
+# chmod +x gitleaks
+# echo "installing gitleaks"
+# mv gitleaks /usr/local/bin/
+
+# cp scripts/pre-commit.py ../../.git/hooks/pre-commit
+# chmod +x ../../.git/hooks/pre-commit
+# git config --global --bool hooks.gitleaks true
+
+# echo 'gitleaks is installed'
